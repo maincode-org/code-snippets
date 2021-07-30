d369c49fe6f530266509de94f05c220bb71bfbaa
Mathias Høyrup Nielsen <33873803+MathiasHoeyrup@users.noreply.github.com>
2021-07-30 12:38:29 +0200

    Create component-boilerplate.tsx

---
+++ frontend/component-boilerplate.tsx
@@ -0,0 +1,12 @@
+/** Describe the purpose of the component here.*/
+
+type IProps = {
+    dummy: string;
+}
+
+const BoilerPlate: React.FC<IProps> = ({ dummy }) => {
+    return (
+      <div></div>
+    );
+}
+export default BoilerPlate;
