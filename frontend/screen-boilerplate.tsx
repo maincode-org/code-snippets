a500a74efab8a5ee4b284e57ec154869f9bc2545
Mark K <33289461+MarkKragerup@users.noreply.github.com>
2021-08-23 12:58:31 +0200

    Update screen-boilerplate.tsx

--- frontend/screen-boilerplate.tsx
+++ frontend/screen-boilerplate.tsx
@@ -2,12 +2,10 @@ import { IonPage, IonContent } from '@ionic/react';
 
 const BoilerPlate: React.FC = () => {
   return (
-    <div>
       <IonPage>
         <IonContent fullscreen>
         </IonContent>
       </IonPage>
-    </div>
   );
 }
 export default BoilerPlate;
